7f20d7273cbdd1dcfaad6dbc0ddef2349f0a7475
Dillon Zhang <d1998z@gmail.com>
2015-12-28 02:33:41 -0500

    Parental Signup Completed and Tested, Parental Signin Completed but not Tested

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+import database_utils
 
 app = Flask(__name__)
 
@@ -8,7 +9,24 @@ def home():
     return render_template("home.html")
     
 
+@app.route("/parentcreate", methods=['GET', 'POST'])
+def parent_create():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        repeat_password = request.form['repeat_password']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email'].lower()
+        result = database_utils.valid_create_parent(username, password, repeat_password, first_name, last_name, email)
+        if result[0]:
+            return result[1]
+        else:
+            return "Error:" + result[1]
+    else:
+        return render_template("parentcreate.html")
+
 if __name__== "__main__":
     app.debug = True
     app.secret_key = "Password"
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0', port=8000)
---
+++ database_utils.py
@@ -0,0 +1,82 @@
+import sqlite3  # Database
+
+from hashlib import sha512  # Hashing for Passwords
+from uuid import uuid4  # Salting for Passwords
+
+import utils  # Minor Utilities
+
+
+# Parent Database -------------------------------------------------------------------------------------------------------------------------------------------
+"""
+Parent Database - Stores parents login information
++-----------+----------+----------+------+------------+-----------+-------+
+| Parent_ID | Username | Password | Salt | First Name | Last Name | Email |
++-----------+----------+----------+------+------------+-----------+-------+
+| INT       | TEXT     | INT      | INT  | TEXT       | TEXT      | TEXT  |
++-----------+----------+----------+------+------------+-----------+-------+
+"""
+
+
+# Validates a parent logging in
+# Returns a Boolean
+# True if successful, False if unsuccessful
+def valid_parent_login(username, password):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    q = 'SELECT name FROM sqlite_master WHERE TYPE = "table" AND NAME = "parent_database"'
+    c.execute(q)
+    if not c.fetchone():
+        conn.close()
+        return False
+    q = 'SELECT password, salt FROM parent_database WHERE username = ?'
+    pepper_and_salt = c.execute(q).fetchone()
+    conn.close()
+    if not pepper_and_salt or sha512((password + pepper_and_salt[1]) * 10000).hexdigest() != pepper_and_salt[0]:
+        return False
+    return True
+
+
+# Adds a parent to the database
+# Returns a list [Boolean, String]
+# True if successful, False if unsuccessful
+# String is a status message
+def valid_create_parent(username, password, repeat_password, first_name, last_name, email):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    q = 'CREATE TABLE IF NOT EXISTS parent_database (parent_id INT, username TEXT, password INT, salt INT, first_name TEXT, last_name TEXT, email TEXT)'
+    c.execute(q)
+    q = 'SELECT username, email FROM parent_database'
+    users = c.execute(q)
+    valid_data = utils.valid_data(username, password, repeat_password, email, users)
+    if not valid_data[0]:
+        conn.close()
+        return valid_data
+    else:
+        salt = uuid4().hex
+        hash_password = sha512((password + salt) * 10000).hexdigest()
+        q = 'SELECT COUNT(*) FROM parent_database'
+        num_rows = c.execute(q).fetchone()[0]
+        q = 'INSERT INTO parent_database (parent_id, username, password, salt, first_name, last_name, email) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        c.execute(q, (num_rows + 1, username, hash_password, salt, first_name, last_name, email))
+        conn.commit()
+        conn.close()
+        return [True, "Successful Account Creation"]
+
+
+# Teacher Database ------------------------------------------------------------------------------------------------------------------------------------------
+"""
+Teacher Database - Stores parents login information
++------------+----------+----------+------+------------+-----------+-------+
+| Teacher_ID | Username | Password | Salt | First Name | Last Name | Email |
++------------+----------+----------+------+------------+-----------+-------+
+| INT        | TEXT     | INT      | INT  | TEXT       | TEXT      | TEXT  |
++------------+----------+----------+------+------------+-----------+-------+
+"""
+
+
+def valid_teach_login(username, password):
+    return False
+
+
+def valid_create_teacher(username, password, repeat_password, first_name, last_name, email):
+    return False
