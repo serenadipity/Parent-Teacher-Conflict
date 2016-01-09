e3e6aad41a31c092c42f759a38fc89c16feb93ce
serenadipity <serenagchan@gmail.com>
2016-01-09 15:04:30 -0500

    stuff

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
-    
+    return render_template("home.html")   
 
 @app.route("/parentcreate", methods=['GET', 'POST'])
 def parent_create():
