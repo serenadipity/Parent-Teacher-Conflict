059b090ae89692a17323de9feb2a93494b1d794a
Sammi <sammiwuleung@gmail.com>
2016-01-23 14:49:38 -0500

    teacher create updated to have select schools

--- static/style.js
+++ static/style.js
@@ -1,3 +1,10 @@
-/* Javascript sheet */
-
-
+$( "select" ).change(function() {
+    var text = $( "#school option:selected" ).text();
+    if (text == "Other") {
+	$("#other").html("<input type='text' name='school' class='form-control input-lg' placeholder='Input Your School'  required>");
+    }
+    else {
+	$("#other").html("");
+    }
+    console.log(text);
+});
