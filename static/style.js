1eacd8215d5f6e23ffd2c3e6c6e294e606ecd6ef
Sammi <sammiwuleung@gmail.com>
2016-01-23 16:00:33 -0500

    minor change again

--- static/style.js
+++ static/style.js
@@ -1,10 +1,9 @@
 $( "select" ).change(function() {
     var text = $( "#school option:selected" ).text();
     if (text == "Other") {
-	$("#other").html("<input type='text' name='school' class='form-control input-lg' placeholder='Input Your School'  required>");
+	$("#other").html("<input type='text' name='other' class='form-control input-lg' placeholder='Input Your School'  required>");
     }
     else {
 	$("#other").html("");
     }
-    console.log(text);
 });
