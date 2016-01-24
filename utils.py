be13d4d4c0978409643565e95e080b1361579fb1
Dillon Zhang <d1998z@gmail.com>
2016-01-24 15:36:58 -0500

    drawing

--- utils.py
+++ utils.py
@@ -19,3 +19,32 @@ def valid_data(username, password, repeat_password, email, users):
     return [True, "Your information has been validated."]
 
 
+#[[PID, TIME, SECTION #, F, L]]
+def createSchedule(listOfThings):
+    if not(listOfThings and listOfThings[0]):
+        return ""
+    hour = 5
+    minutes = 30
+    if listOfThings[0][1] == 'afternoon':
+        hour = 1
+        minutes = 0
+    stuffToWorkWith = {}
+    for alist in listOfThings:
+        stuffToWorkWith[alist[2]] = alist[3:]
+    stringer = ""
+    stringer += "<table>\n"
+    stringer += "<tr> <th> TIME </th> <th colspan='2'> Parent Name </th> </tr>\n"
+    for counter in range(50):
+        stringer += "<tr>"
+        a = (hour + (minutes + counter * 3) / 60) / 10
+        b = (hour + (minutes + counter * 3) / 60) % 10
+        c = (minutes + counter * 3) / 10
+        d = (minutes + counter * 3) % 10
+        stringer += "<td> %d%d:%d%d </td>" % (a, b, c, d)
+        addition = "<td></td><td></td>"
+        if counter in stuffToWorkWith.keys():
+            addition = "<td> %s </td><td> %s </td>" % (stuffToWorkWith[counter][0], stuffToWorkWith[counter][1])
+        stringer += addition
+        stringer += "</tr>\n"
+    stringer += "</table\n"
+    return stringer
