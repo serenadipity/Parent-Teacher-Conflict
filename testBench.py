5157729c1ac8a91030bedb620b2fe381dfea78fd
Peter <peterstrbik97@gmail.com>
2016-01-23 20:24:57 -0500

    testing stuff

--- testBench.py
+++ testBench.py
@@ -19,9 +19,24 @@ class testBench(unittest.TestCase):
         self.assertEqual(r[0], False)
     def test4_parent_login(self):
         r = database_utils.valid_parent_login("Alpha1", "Aa1!Aa1!")
-        self.assertEqual(r, True)
+        self.assertEqual(r, 1)
     def test5_parent_login(self):
         r = database_utils.valid_parent_login("Alpha1", "Aa1!Aa1!1")
+        self.assertEqual(r, -1)
+    def test6_create_teacher(self):
+        r = database_utils.valid_create_teacher("Alpha2", "Aa2!Aa2!", "Aa2!Aa2!", "Alpha", "2", "Alpha2@hotmail.com")
+        self.assertEqual(r[0], True)
+    def test7_create_teacher(self):
+        r = database_utils.valid_create_teacher("Beta1", "Aa1!Aa1!", "Aa1!Aa1!2", "Beta", "1", "Beta1@hotmail.com")
+        self.assertEqual(r[0], False)
+    def test8_create_teacher(self):
+        r = database_utils.valid_create_teacher("Beta1", "Aa1!Aa1!", "Aa1!Aa1!", "Beta", "1", "Alpha2@hotmail.com")
+        self.assertEqual(r[0], False)
+    def test9_teacher_login(self):
+        r = database_utils.valid_teacher_login("Alpha2", "Aa2!Aa2!")
+        self.assertEqual(r, True)
+    def test10_teacher_login(self):
+        r = database_utils.valid_teacher_login("Alpha2", "Aa2!Aa2!2")
         self.assertEqual(r, -1)
 
 if __name__ == "__main__":
